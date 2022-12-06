6f4e4a0fadfaa00a7092b04614f4eede52b558b1
Gustavo Molina Soares <97996136+guzsoares@users.noreply.github.com>
2022-12-06 17:03:49 -0300

    organizando

--- inf1022_compiler/cProgram.c
+++ inf1022_compiler/cProgram.c
@@ -6,15 +6,10 @@ scanf("%d",&X);
 int Y;
 printf("Entrada [Y]: ");
 scanf("%d",&Y);
-X = X + 1;
-X = Y;
-if (X != 0) {
-X = 0;
-}
-for (int i = 0; i < Y; i++) {
+while (Y > 0) {
+Y = Y - 1;
 X = X * 2;
 }
-X = X * X;
 printf("Saida: [X] = %d \n", X);
 return 0;
 }
